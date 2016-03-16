d492174c951739d05a5d5b05c07a61b3f40a79c5
John Julien <john@julienfamily.com>
2016-03-15 20:24:55 -0500

    Adding julien.sh

---
+++ profile.d/julien.sh
@@ -0,0 +1,14 @@
+set -o vi
+
+#Setup PS1 Prompt
+PS1='
+\u@\h [$PWD]
+\$ '
+
+alias cdu='cd /usr/local/bin'
+alias ls='ls --color=auto'
+alias ll='ls -al'
+alias cdm='cd /var/spool/mail'
+alias cdl='cd /var/log'
+alias suroot="sudo su -"
+alias vi='vim'
